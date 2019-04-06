6393e9ca6256b2019c695722cbab30a3c705e423
John <John@Apple-Computer.home>
2019-04-06 13:46:42 -0400

    Can run from bin/open

--- lib/phish_book.rb
+++ lib/phish_book.rb
@@ -1,4 +1,9 @@
-require "phish_book/version"
+require_relative "phish_book/version"
+require_relative "phish_book/cli"
+
+require "nokogiri"
+require "open-uri"
+require "pry"
 
 module PhishBook
   class Error < StandardError; end
---
+++ lib/phish_book/cli.rb
@@ -0,0 +1,8 @@
+class PhishBook::CLI
+
+    def greeting
+    puts "WELCOME TO PHISH BOOK"
+    puts "WHERE FANS CAN TRY TO REMEMBER ALL THE THINGS THEY FORGOT"
+    end
+
+end
