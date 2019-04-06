78d28eea4bf665e42820352b35d5bef23ace105a
John <John@Apple-Computer.home>
2019-04-06 13:50:23 -0400

    Can run from bin/open

--- lib/phish_book.rb
+++ lib/phish_book.rb
@@ -1,5 +1,11 @@
 require_relative "phish_book/version"
 require_relative "phish_book/cli"
+require_relative "phish_book/fan"
+require_relative "phish_book/scraper"
+require_relative "phish_book/show"
+require_relative "phish_book/song"
+require_relative "phish_book/state"
+require_relative "phish_book/year"
 
 require "nokogiri"
 require "open-uri"
